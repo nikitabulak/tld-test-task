bc7140de1361820b58098f4d119c1b61a9325d83
Nikita <nikita_bulak@mail.ru>
2024-04-30 12:45:43 +0300

    template

---
+++ src/main/java/bul/nik/tldtesttask/TldTestTaskApplication.java
@@ -0,0 +1,13 @@
+package bul.nik.tldtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TldTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TldTestTaskApplication.class, args);
+	}
+
+}
